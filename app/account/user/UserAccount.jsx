d68a20fa7f75fe781297fbfc800fa344837d4551
JovanJJ <jovanjj99@gmail.com>
2026-02-05 00:08:01 +0100

    Fixed user acc page bug, adding loading.js feature.

--- app/account/user/UserAccount.jsx
+++ app/account/user/UserAccount.jsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-import Star from "../../../public/stars.avif";
-import Link from "next/link";
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../../../lib/auth-config';
 import { headers } from 'next/headers';
+import UserAccountPage from '../../components/UserAccountPage';
 
 const noImage = 'https://upload.wikimedia.org/wikipedia/commons/a/ad/Placeholder_no_text.svg';
 const profileImage = "https://upload.wikimedia.org/wikipedia/commons/a/ad/Placeholder_no_text.svg"
@@ -25,52 +23,8 @@ export default async function UserAccount () {
       }
     });
     const data = await res.json();
-    const profileImage = data.data.profileImage;
     
-    return (
-        profileImage &&
-        <section className="w-full flex justify-center pb-24 pt-15">
-            <div className="mx-auto max-w-[1200px] flex flex-col xl:flex-row gap-7 mt-[30px] w-full px-4">
-
-
-                <div className="mx-auto w-full max-w-[1000px] p-6 rounded-3xl shadow-xl bg-blue-50  border border-blue-200">
-                    <h1 className="text-3xl pb-3">Nalog</h1>
-                    <Link href="settings">Klikni <span className="underline">Ovde</span> za podešavanje naloga</Link>
-
-                    <div className="w-full flex justify-center pt-7">
-                        <div className="w-2/3 max-w-48 aspect-square rounded-full relative border-3 border-blue-200">
-                            <Image
-                                src={profileImage === null ? noImage : profileImage}
-                                fill
-                                alt="Profile Image"
-                                className="w-full rounded-full object-cover"
-                            />
-                            <div className="absolute -bottom-6 -right-6 z-30 sm:-bottom-5 sm:-right-5 md:-bottom-1 md:-right-5 lg:bottom-1 lg:right-1">
-                                <Image src={Star} alt="star" className="w-[50px] h-[50px]" />
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <div className="flex flex-col gap-4 mx-auto max-w-[600px] text-[18px] pb-10 sm:text-xl pt-10">
-                        <div className="flex justify-between items-end border-b pt-10 border-gray-100 pb-1">
-                            <span className="italic text text-base">Ime:</span>
-                            <span>{data.data.name}</span>
-                        </div>
-                        
-                        
-                        
-                        <div className="flex justify-between items-end border-b border-gray-100 pb-1">
-                            <span className="italic text text-base">E-mail:</span>
-                            <span>{data.data.email}</span>
-                        </div>
-                        
-                    </div>
-
-
-                    
-                </div>
-            </div>
-        </section>
+    return (  
+        <UserAccountPage data={data} />
     );
 }

78314ce504eef95b2567728d6f41b535aaa2bcb1
JovanJJ <jovanjj99@gmail.com>
2026-02-05 00:17:50 +0100

    Fixing user acc error.

--- app/account/user/UserAccount.jsx
+++ app/account/user/UserAccount.jsx
@@ -6,25 +6,30 @@ import UserAccountPage from '../../components/UserAccountPage';
 const noImage = 'https://upload.wikimedia.org/wikipedia/commons/a/ad/Placeholder_no_text.svg';
 const profileImage = "https://upload.wikimedia.org/wikipedia/commons/a/ad/Placeholder_no_text.svg"
 
-export default async function UserAccount () {
+export default async function UserAccount() {
     const session = await getServerSession(authOptions);
     const role = session?.user.role;
     const id = session?.user.id;
-    
+
     const headersList = await headers();
     const host = headersList.get('host') || 'localhost:3000';
     const protocol = headersList.get('x-forwarded-proto') || 'http';
     const baseUrl = `${protocol}://${host}`;
-    
-    const res = await fetch(`${baseUrl}/api/current-user?role=${role}&id=${id}`, { 
-      cache: 'no-store',
-      headers: {
-        'Cookie': headersList.get('cookie') || '',
-      }
-    });
-    const data = await res.json();
-    
-    return (  
+
+    const data = null;
+    if (role && id) {
+        const res = await fetch(`${baseUrl}/api/current-user?role=${role}&id=${id}`, {
+            cache: 'no-store',
+            headers: {
+                'Cookie': headersList.get('cookie') || '',
+            }
+        });
+        data = await res.json();
+    }
+
+
+
+    return (
         <UserAccountPage data={data} />
     );
 }

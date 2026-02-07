4b6c62d9dc2ab8721731c2f1b978879b460a8f1a
JovanJJ <jovanjj99@gmail.com>
2026-02-07 15:20:42 +0100

    Fixing loading animation language missmatch

--- app/[lang]/(main)/profile/[id]/Loading.js
+++ app/[lang]/(main)/profile/[id]/Loading.js
@@ -1,10 +1,10 @@
 
 
-export default function Loading({ lang }) {
+export default function Loading() {
     return (
         <div className="min-h-screen w-full flex items-center justify-center">
             <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-blue-500"></div>
-            <p className="ml-4">{lang === 'en' ? 'Loading...' : 'Učitava se...'}</p>
+            <p className="ml-4">Loading...</p>
         </div>
     );
 }
--- app/[lang]/settings/Loading.js
+++ app/[lang]/settings/Loading.js
@@ -4,7 +4,7 @@ export default function Loading() {
     return (
         <div className="min-h-screen w-full flex items-center justify-center">
             <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-blue-500"></div>
-            <p className="ml-4">Učitava se...</p>
+            <p className="ml-4">Loading...</p>
         </div>
     );
 }
